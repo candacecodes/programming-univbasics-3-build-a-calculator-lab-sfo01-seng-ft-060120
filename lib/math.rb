386163d25affe69f3859b0ae6ae7eb9b4c820f52
eager-class-5814 <candacehchoe@gmail.com>
2020-05-15 21:41:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,14 +8,14 @@ num1-num2
 
 def division(num1, num2)
 
-num1%num2
+num1 / num2
 
 def multiplication(num1, num2)
 
 num1 * num2
 
 def modulo(num1, num2)
-num1, num2
+num1 % num2
 end
 
 def square_root(num)
